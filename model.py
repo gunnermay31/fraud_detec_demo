fb2617c5b899d1b03405c886975082c9ea92f3a7
gunnermay31 <ankurmay31@gmail.com>
2025-03-09 22:59:38 +0530

    added 3 new models for the code

--- model.py
+++ model.py
@@ -9,3 +9,13 @@ def sub(a,b):
  return a - b
 
 print(sub(a,b))
+
+def model1():
+ print("adding code for the model 1")
+
+ def model2():
+   print("adding code for the model 1")
+
+def model3():
+ print("adding code for the model 1")
+ 
